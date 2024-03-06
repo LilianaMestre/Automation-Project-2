6da3c96c54334ab9850413912d3a70ba39450940
LilianaMestre <bitinha@gmail.com>
2024-03-06 17:27:01 +0000

    My commets about the commit

--- cypress/e2e/issue-time-tracking.cy.js
+++ cypress/e2e/issue-time-tracking.cy.js
@@ -1,115 +1,162 @@
-describe("Time estimation and logging functionality", () => {
-  const issueDetailsSelector = '[data-testid="modal:issue-details"]';
-  const trackingModalSelector = '[data-testid="modal:tracking"]';
-  const originalEstimateInputSelector = ".sc-dxgOiQ.HrhWu";
-  const timeTrackingSectionSelector = ".sc-rBLzX.irwmBe";
-  const closeButtonSelector = '[data-testid="icon:close"]';
-  const doneButtonSelector = 'button:contains("Done")';
-  const numberInputSelector = 'input[placeholder="Number"]';
-  const initialEstimatedHours = "70";
-  const updatedEstimatedHours = "68";
-  const initialTimeSpent = "96";
-  const updatedTimeSpent = "69";
-  const initialTimeRemaining = "169";
-  const updatedTimeRemaining = "170";
+import {
+  issueCreatedConfirmation,
+  issueDescription,
+  issueTitle,
+} from "../constants/constants";
+import {
+  backLogList,
+  inputFieldTime,
+  timeTrackingModal,
+  timeTrackingButton,
+} from "../pages/selectors";
 
+const estimatedTime = "10";
+const estimatedTimeUpdated = "20";
+const loggedTime = "2";
+const remainingTime = "5";
+const loggedTimeUpdated = "3";
+const remainingTimeUpdated = "4";
+
+const estimatedTimeExpectedText = "h estimated";
+const loggedTimeExpectedText = "h logged";
+const remainingTimeExpectedText = "h remaining";
+
+const shouldShowNoTimeLogged = true;
+const shouldNotShowEstimatedTime = false;
+
+describe("Time-tracking functionality tests of the issue", () => {
   beforeEach(() => {
     cy.visit("/");
     cy.url()
-      .should("include", `${Cypress.env("baseUrl")}project`)
+      .should("eq", `${Cypress.env("baseUrl")}project/board`)
       .then((url) => {
-        cy.visit(`${url}/board`);
-        cy.contains("This is an issue of type: Task.").click();
+        cy.visit(url + "/board?modal-issue-create=true");
+
+        // Due to unknown Jira bug we need to create new issue to update time
+        cy.get('[data-testid="modal:issue-create"]').within(() => {
+          cy.get('[data-testid="select:type"]').click();
+          cy.get('[data-testid="select-option:Bug"]').click();
+          cy.get(".ql-editor").type(issueDescription);
+          cy.get('input[name="title"]').type(issueTitle);
+          cy.get('[data-testid="select:userIds"]').click();
+          cy.get('[data-testid="select-option:Lord Gaben"]').click();
+          cy.get('button[type="submit"]').click();
+        });
+
+        cy.contains(issueCreatedConfirmation).should("be.visible");
+        cy.get(backLogList).should("be.visible").contains(issueTitle).click();
       });
   });
 
-  it.only("Should add, edit, and remove time estimation", () => {
-    // Adding Estimation
-    cy.get(issueDetailsSelector).within(() => {
-      cy.get(originalEstimateInputSelector)
-        .click()
-        .clear()
-        .type(initialEstimatedHours)
-        .type("{enter}");
-    });
+  it("Should add, update and remove estimated time in the issue", () => {
+    validateNoTimeLogged(shouldShowNoTimeLogged);
+    cy.get(inputFieldTime).type(estimatedTime);
+    cy.get(inputFieldTime).should("have.value", estimatedTime);
+    validateTime(estimatedTime, estimatedTimeExpectedText);
 
-    // Asserting the adding time estimation
-    cy.get(originalEstimateInputSelector).should(
-      "have.value",
-      initialEstimatedHours
-    );
-    cy.get(timeTrackingSectionSelector).should(
-      "contain",
-      initialEstimatedHours
-    );
-    cy.get(closeButtonSelector).click({ multiple: true }).should("not.exist");
+    cy.get(inputFieldTime).clear().type(estimatedTimeUpdated);
+    cy.get(inputFieldTime).should("have.value", estimatedTimeUpdated);
+    validateTime(estimatedTimeUpdated, estimatedTimeExpectedText);
 
-    // Editing Estimation
-    cy.get(issueDetailsSelector).within(() => {
-      cy.get(originalEstimateInputSelector)
-        .click()
-        .clear()
-        .type(updatedEstimatedHours)
-        .type("{enter}");
-      cy.get(originalEstimateInputSelector).should(
-        "have.value",
-        updatedEstimatedHours
-      );
-      cy.get(timeTrackingSectionSelector).should(
-        "contain",
-        updatedEstimatedHours
-      );
-      cy.get(closeButtonSelector).click();
-    });
-
-    // Deleting Estimation
-    cy.get(issueDetailsSelector).within(() => {
-      cy.get(originalEstimateInputSelector).click().clear().type("{enter}");
-      cy.get(originalEstimateInputSelector).should("be.empty");
-      cy.get(timeTrackingSectionSelector).should("contain", "No Time Logged");
-      cy.get(closeButtonSelector).click();
-    });
+    cy.get(inputFieldTime).click().clear();
+    validateNoTimeLogged(shouldShowNoTimeLogged);
   });
 
-  it("Should add, edit, and remove time spent and remaining", () => {
-    // Adding Time Spent and Remaining
-    cy.get(issueDetailsSelector).click();
-    cy.get(trackingModalSelector).within(() => {
-      cy.get(numberInputSelector).first().clear().type(initialTimeSpent);
-      cy.get(numberInputSelector).eq(1).clear().type(initialTimeRemaining);
-      cy.get(doneButtonSelector).click();
-    });
-    // Asserting after adding
-    cy.get(timeTrackingSectionSelector)
-      .should("contain", initialTimeSpent)
-      .and("contain", "logged");
-    cy.get(timeTrackingSectionSelector)
-      .should("contain", initialTimeRemaining)
-      .and("contain", "remaining");
+  it("Should add and remove logged time values", () => {
+    const shouldClearTime = true;
 
-    // Editing Time Spent and Remaining
-    cy.get(issueDetailsSelector).click();
-    cy.get(trackingModalSelector).within(() => {
-      cy.get(numberInputSelector).first().clear().type(updatedTimeSpent);
-      cy.get(numberInputSelector).eq(1).clear().type(updatedTimeRemaining);
-      cy.get(doneButtonSelector).click();
-    });
-    // Asserting after editing
-    cy.get(timeTrackingSectionSelector)
-      .should("contain", updatedTimeSpent)
-      .and("contain", "logged");
-    cy.get(timeTrackingSectionSelector)
-      .should("contain", updatedTimeRemaining)
-      .and("contain", "remaining");
+    cy.get(inputFieldTime).type(estimatedTime);
+    cy.get(inputFieldTime).should("have.value", estimatedTime);
+    validateTime(estimatedTime, estimatedTimeExpectedText);
 
-    // Deleting Time Spent and Remaining
-    cy.get(issueDetailsSelector).click();
-    cy.get(trackingModalSelector).within(() => {
-      cy.get(numberInputSelector).first().clear();
-      cy.get(numberInputSelector).eq(1).clear();
-      cy.get(doneButtonSelector).click();
-    });
-    // Asserting after deleting
-    cy.get(timeTrackingSectionSelector).should("contain", "No time logged");
+    // Add initial values to logged and remaining times and validate values after
+    openTimeTrackingAndChangeLoggedTime(loggedTime, remainingTime);
+
+    validateTime(loggedTime, loggedTimeExpectedText);
+    validateTime(remainingTime, remainingTimeExpectedText);
+    validateTime(
+      estimatedTime,
+      estimatedTimeExpectedText,
+      shouldNotShowEstimatedTime
+    );
+    validateNoTimeLogged();
+
+    // Edit initial values to logged and remaining times and validate values after
+    openTimeTrackingAndChangeLoggedTime(
+      loggedTimeUpdated,
+      remainingTimeUpdated
+    );
+
+    validateTime(loggedTimeUpdated, loggedTimeExpectedText);
+    validateTime(remainingTimeUpdated, remainingTimeExpectedText);
+    validateTime(
+      estimatedTime,
+      estimatedTimeExpectedText,
+      shouldNotShowEstimatedTime
+    );
+    validateNoTimeLogged();
+
+    // Clear the values from logged and remaining times and validate values after
+    openTimeTrackingAndChangeLoggedTime(
+      loggedTime,
+      remainingTime,
+      shouldClearTime
+    );
+
+    validateTime(
+      loggedTime,
+      loggedTimeExpectedText,
+      shouldNotShowEstimatedTime
+    );
+    validateTime(
+      remainingTime,
+      remainingTimeExpectedText,
+      shouldNotShowEstimatedTime
+    );
+    validateTime(estimatedTime, estimatedTimeExpectedText);
+    validateNoTimeLogged(shouldShowNoTimeLogged);
   });
 });
+
+function validateTime(
+  timeValue,
+  remainingPartOfString,
+  shouldBeVisible = true
+) {
+  if (shouldBeVisible) {
+    cy.contains(`${timeValue}${remainingPartOfString}`).should("be.visible");
+  } else {
+    cy.contains(`${timeValue}${remainingPartOfString}`).should("not.exist");
+  }
+}
+
+function openTimeTrackingAndChangeLoggedTime(
+  loggedTime,
+  remainingTime,
+  shouldClearTime = false
+) {
+  cy.get(timeTrackingButton).click();
+  cy.get(timeTrackingModal)
+    .should("be.visible")
+    .within(() => {
+      if (shouldClearTime) {
+        cy.get(inputFieldTime).eq(0).clear();
+        cy.get(inputFieldTime).eq(1).clear();
+      } else {
+        cy.get(inputFieldTime).eq(0).type(loggedTime);
+        cy.get(inputFieldTime).eq(1).type(remainingTime);
+      }
+
+      cy.contains("button", "Done").click();
+    });
+
+  cy.get(timeTrackingModal).should("not.exist");
+}
+
+function validateNoTimeLogged(shouldShowNoTimeLogged = false) {
+  if (shouldShowNoTimeLogged) {
+    cy.contains("No time logged").should("be.visible");
+  } else {
+    cy.contains("No time logged").should("not.exist");
+  }
+}
